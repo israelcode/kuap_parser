d28ce719a0f198ab7d92a6b255db89bd742aa7ae
israelcode <31614943+israelcode@users.noreply.github.com>
2018-03-30 21:25:28 +0300

    Update and rename hello_kuap.html to hello_kuap.js

---
+++ hello_kuap.js
@@ -0,0 +1,42 @@
+<script>
+(function() {
+var jsonArr = []; // array of Json structures
+
+
+    var lenghtStrings = document.getElementsByTagName('tr');
+    console.log(lenghtStrings.length);
+
+    for(i = 0, l = lenghtStrings.length; i < l; i++) {
+	
+        var currentString = lenghtStrings[i];
+        var lenghtColumns = currentString.querySelectorAll('td');
+		
+        console.log(currentString.textContent);
+		
+        for(j = 1, m = lenghtColumns.length; j < m-1; j++) {
+		
+			var obj = {}; // object that we'll put in jsonArr
+			
+			var head = lenghtColumns[0].innerText; // Заголовок строчки (0-ая колонка)
+			//alert(head.trim());
+			head=head.trim();
+			console.log(head);
+			var currentColumn = lenghtColumns[j];
+            console.log(currentColumn.textContent);
+			
+			obj[head] = currentColumn.textContent;
+			jsonArr.push(obj);
+			//alert(obj);
+		
+			
+        }
+    }
+	document.write("<br>"+JSON.stringify(jsonArr));
+
+
+})();
+
+</script>
+			
+</body>
+</html>
